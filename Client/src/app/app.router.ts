6abbecdefd9c5a6ed6c3a24334f61c1bd745cd4d
rishav_goswami <Rishav_Goswami@Dell.com>
2020-10-17 03:58:14 +0530

    Fixed routing issue

--- Client/src/app/app.router.ts
+++ Client/src/app/app.router.ts
@@ -1,32 +1,28 @@
-import { FeedComponent } from "./feed/feed.component";
-import { Routes, RouterModule } from '@angular/router'
-import { ProfileComponent } from "./profile/profile.component";
-import { BlogComponent } from "./blog/blog.component";
-import { AdminConsoleComponent } from "./admin/admin-console/admin-console.component";
-import { PrivacyComponent } from "./privacy/privacy.component";
-import { AccountComponent } from "./account/account.component";
-import { SignupComponent } from "./signup/signup.component";
+import {FeedComponent} from "./feed/feed.component";
+import {RouterModule, Routes} from '@angular/router'
+import {ProfileComponent} from "./profile/profile.component";
+import {BlogComponent} from "./blog/blog.component";
+import {AdminConsoleComponent} from "./admin/admin-console/admin-console.component";
+import {PrivacyComponent} from "./privacy/privacy.component";
+import {AccountComponent} from "./account/account.component";
+import {SignupComponent} from "./signup/signup.component";
 
 const appRoutes: Routes = [
-    { path: '', component: FeedComponent },
-    { path: 'feed', component: FeedComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'blog', children:[
-        {path:':id',component:BlogComponent},
-        {path:'',component:BlogComponent}
-    ] },
-    { path: 'account', component: AccountComponent },
-    { path: 'signup', component: SignupComponent },
-    // { path: 'blog', component: BlogComponent },
-    { path: 'privacy', component: PrivacyComponent },
-    {
-        path: 'admin',
-        children: [
-            { path: '', component: AdminConsoleComponent }
-        ]
-    },
+  {path: '', pathMatch: 'full', component: FeedComponent},
+  {path: 'admin', pathMatch: 'full', component: AdminConsoleComponent},
+  {path: 'feed', pathMatch: 'full', component: FeedComponent},
+  {path: 'profile', pathMatch: 'full', component: ProfileComponent},
+  {
+    path: 'blog', pathMatch: 'full', children: [
+      {path: ':id', component: BlogComponent},
+      {path: '', component: BlogComponent}
+    ]
+  },
+  {path: 'account', pathMatch: 'full', component: AccountComponent},
+  {path: 'signup', pathMatch: 'full', component: SignupComponent},
+  {path: 'privacy', pathMatch: 'full', component: PrivacyComponent},
 
-    { path: '**', redirectTo: '' }
+  {path: '**', redirectTo: ''}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
